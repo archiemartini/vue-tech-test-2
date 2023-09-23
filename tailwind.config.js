7296415cf349924ba36a1f110af8f0cd162327b9
Archie Martin <archie.cgm@gmail.com>
2023-09-23 11:28:10 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.vue', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
